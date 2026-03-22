ddb73fc112396e90ae3fe8b1c02d0119e0808ddc
fixedbits <fixingbitbybit@gmail.com>
2026-03-21 23:38:32 -0600

    Simple background color to Body

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
+import { useState } from "react";
 import "./styles/app.css";
 
+import Balance from "./components/Balance";
+import TransactionForm from "./components/TransactionForm";
+import TransactionList from "./components/TransactionList";
+
 function App() {
-  return <div className="app">Financial Tracker</div>;
+  const [transactions, setTransactions] = useState([]);
+
+  const addTransaction = (transaction) => {
+    setTransactions((prev) => [...prev, transaction]);
+  };
+
+  const totalBalance = transactions.reduce((sum, t) => sum + Number(t.amount), 0);
+
+  return (
+    <div className="app">
+      <h1>Financial Tracker</h1>
+
+      <Balance total={totalBalance} />
+      <TransactionForm onAdd={addTransaction} />
+      <TransactionList items={transactions} />
+    </div>
+  );
 }
 
 export default App;
