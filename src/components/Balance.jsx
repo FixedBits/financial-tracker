ccddde2a437955933d87bbd86d7c19c22b1d2857
fixedbits <fixingbitbybit@gmail.com>
2026-05-03 18:01:29 -0600

    Update Balance.jsx

--- src/components/Balance.jsx
+++ src/components/Balance.jsx
@@ -1,8 +1,8 @@
-function Balance({ total }) {
+function Balance({total}) {
   return (
     <div className="balance">
       <h2>Balance</h2>
-      <p className={`amount ${total >= 0 ? "positive" : "negative"}`}>{total.toLocaleString(undefined, { minimumFractionDigits: 2 })}</p>
+      <p className={`amount ${total >= 0 ? "positive" : "negative"}`}>{total.toLocaleString(undefined, {minimumFractionDigits: 2})}</p>
     </div>
   );
 }
